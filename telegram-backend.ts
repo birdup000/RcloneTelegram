e6b628daa19f2d95d4794ec2233151db1767ae95
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-24 16:06:14 -0500

    revise

--- telegram-backend.ts
+++ telegram-backend.ts
@@ -1,5 +1,5 @@
 import { Readable, Writable } from 'stream';
-import { Telegraf, Context, Update } from 'telegraf';
+import { Telegraf } from 'telegraf';
 import crypto from 'crypto';
 import fetch from 'node-fetch';
 import { Config } from 'rclone';
@@ -7,7 +7,7 @@ import { Config } from 'rclone';
 class TelegramBackend {
   private config: any;
   private botToken: string;
-  private bot: Telegraf<Context<Update>>['bot'];
+  private bot: any;
 
   constructor(config: Config) {
     this.config = config.get('telegram');
@@ -20,7 +20,7 @@ class TelegramBackend {
       throw new Error('Invalid telegram bot_token');
     }
 
-    this.bot = new Telegraf(this.botToken).bot;
+    this.bot = new Telegraf(this.botToken);
   }
 
   async Put(inputStream: Readable, out: any, options: any) {
@@ -34,14 +34,12 @@ class TelegramBackend {
     let part = 0;
     let checksum = '';
 
-    const chunkSize = 2 * 1024 * 1024 * 1024; // 2GB
+    const chunkSize = 2 * 1024 * 1024; // 2MB
     const chunks = [];
 
-    // Read the input stream into chunks of 2GB
-    while (true) {
-      const buffer = inputStream.read(chunkSize);
-      if (!buffer) break;
-      chunks.push(buffer);
+    // Read the input stream into chunks of 2MB
+    for await (const chunk of inputStream) {
+      chunks.push(chunk);
     }
 
     // Upload each chunk separately to Telegram
@@ -50,7 +48,7 @@ class TelegramBackend {
       part++;
       checksum += crypto.createHash('sha256').update(chunk).digest('hex');
 
-      const msg = await this.bot.sendDocument(chatId, { document: chunk });
+      const msg = await this.bot.telegram.sendDocument(chatId, { source: chunk });
       msgIds.push(msg.message_id);
     }
 
@@ -58,17 +56,46 @@ class TelegramBackend {
   }
 
   async Get(outStream: Writable, inPath: string, options: any) {
-    const chatId = this.config.chat_id;
-    const msgId = this.config[inPath];
-    if (!msgId) {
-      throw new Error(`Invalid telegram message id for path: ${inPath}`);
+    if (!outStream.writable) {
+      throw new Error('Output stream is not writable');
     }
 
-    const fileUrl = await this.bot.telegram.getFileLink(msgId);
-    const response = await fetch(fileUrl.href);
+    const chatId = this.config.chat_id;
+    const msgIds = options?.msgIds;
+    if (!msgIds || !Array.isArray(msgIds)) {
+      throw new Error('Invalid options');
+    }
 
-    response.body.pipe(outStream);
+    // Download each chunk separately from Telegram and write to the output stream
+    for (const msgId of msgIds) {
+      const msg = await this.bot.telegram.getFile(chatId, msgId);
+      const url = `https://api.telegram.org/file/bot${this.botToken}/${msg.file_path}`;
+      const response = await fetch(url);
+      const buffer = await response.buffer();
+      outStream.write(buffer);
+    }
+
+    // Merge the downloaded chunks into a single file
+    return { size: outStream.bytesWritten };
+  }
+
+  async Delete(inPath: string, options: any) {
+    const chatId = this.config.chat_id;
+    const msgIds = options?.msgIds;
+    if (!msgIds || !Array.isArray(msgIds)) {
+      throw new Error('Invalid options');
+    }
+
+    for (const msgId of msgIds) {
+      try {
+        await this.bot.telegram.deleteMessage(chatId, msgId);
+      } catch (e) {
+        console.log('Message deletion failed', e);
+      }
+    }
+
+    return { size: 0 };
   }
 }
 
-export default TelegramBackend;
+export { TelegramBackend };
