15bbb71567d8e07175de6236a2b4b30d7089db52
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-26 12:03:03 -0500

    Update telegram-backend.ts

--- telegram-backend.ts
+++ telegram-backend.ts
@@ -1,130 +1,77 @@
-import { Readable, Writable } from 'stream';
+import { Config } from 'rclonejs';
 import { Telegraf } from 'telegraf';
-import { Config } from 'rclone.js';
-import express from 'express';
-import * as node_fetch from 'node-fetch';
 
-// This is a type annotation for the `config` parameter.
-// It specifies that the `config` parameter must be an object
-// with a property called `telegram`.
-interface TelegramConfig {
-  botToken: string;
-  chatId: number;
+const client = new rclonejs.Client();
+const bot = new Telegraf('YOUR_BOT_TOKEN');
+
+const config = {
+  type: 'drive',
+  client_id: 'YOUR_CLIENT_ID',
+  client_secret: 'YOUR_CLIENT_SECRET',
+  token: 'YOUR_TOKEN',
+};
+
+client.connect(config);
+
+// This is a function that sends a file to Telegram.
+async function sendFile(file) {
+  try {
+    const chunks = await client.getChunks(file);
+    for (const chunk of chunks) {
+      await bot.telegram.sendDocument(YOUR_CHAT_ID, { source: chunk });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-class TelegramBackend {
-  private config: TelegramConfig;
-  private bot: Telegraf;
-
-  constructor(config: TelegramConfig) {
-    this.config = config;
-    this.bot = new Telegraf(this.config.botToken);
-  }
-
-  async Put(inputStream: Readable, out: Writable) {
-    if (!inputStream.readable) {
-      throw new Error('Input stream is not readable');
-    }
-
-    let chunks = [];
-    for await (const chunk of inputStream) {
-      chunks.push(chunk);
-    }
-
-    if (chunks.length > 2097152000) {
-      // If the file is larger than 2GB, split it into chunks of 2GB
-      const chunkSize = 2097152000;
-      chunks = chunks.reduce((acc, chunk, i) => {
-        if (i % chunkSize === 0) {
-          acc.push(chunk);
-        } else {
-          acc[acc.length - 1] = Buffer.concat([acc[acc.length - 1], chunk]);
-        }
-        return acc;
-      }, []);
-    }
-
-    await Promise.all(chunks.map(async (chunk) => {
-      await this.bot.telegram.sendDocument(this.config.chatId, { source: chunk });
-    }));
-  }
-
-  async Get(outStream: Writable, inPath: string) {
-    if (!outStream.writable) {
-      throw new Error('Output stream is not writable');
-    }
-
-    const response = await fetch(`https://api.telegram.org/file/bot${this.config.botToken}/${inPath}`);
+// This is a function that gets a file from Telegram.
+async function getFile(file) {
+  try {
+    const response = await fetch(`https://api.telegram.org/file/bot${YOUR_BOT_TOKEN}/${file}`);
     const buffer = await response.arrayBuffer();
-
-    if (buffer.byteLength > 2097152000) {
-      // If the file is larger than 2GB, merge the chunks into a single file
-      const chunkSize = 2097152000;
-      const chunks = [];
-      let offset = 0;
-      while (offset < buffer.byteLength) {
-        chunks.push(buffer.slice(offset, offset + chunkSize));
-        offset += chunkSize;
-      }
-      await Promise.all(chunks.map(async (chunk) => {
-        await outStream.write(chunk);
-      }));
-    } else {
-      await outStream.write(buffer);
-    }
-  }
-
-  async Delete(inPath: string) {
-    await this.bot.telegram.deleteMessage(this.config.chatId, Number(inPath));
-  }
-
-  // This is a function that starts listening for messages from Telegram.
-  start() {
-    this.bot.launch();
-  }
-
-  // This is a function that stops listening for messages from Telegram.
-  async stop() {
-    await this.bot.stop();
-  }
-
-  async upload(file: string, remote: string) {
-    try {
-      const client = new rclone.Client();
-      await client.connect();
-      await client.copy(file, remote, { chunkSize: 2097152000 });
-    } catch (err) {
-      console.log(err);
-    }
+    return buffer;
+  } catch (err) {
+    console.log(err);
   }
 }
 
-// This is a function that starts the server.
-async function startServer() {
-  const app = express();
-
-  app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
-
-  app.post('/upload', async (req, res) => {
-    const file = req.body.file;
-    const remote = req.body.remote;
-
-    try {
-      const client = new rclone.Client();
-      await client.connect();
-      await client.copy(file, remote, { chunkSize: 2097152000 });
-      res.sendStatus(200);
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
-  });
-
-  app.listen(3000, () => {
-    console.log('App listening on port 3000');
-  });
+// This is a function that lists the files in a chat.
+async function listFiles(chatId) {
+  try {
+    const response = await fetch(`https://api.telegram.org/getUpdates?chat_id=${chatId}`);
+    const data = await response.json();
+    const files = data.result.messages.map(message => message.document);
+    return files;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-startServer();
+// This is a function that deletes a file from Telegram.
+async function deleteFile(file) {
+  try {
+    await bot.telegram.deleteMessage(YOUR_CHAT_ID, Number(file));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// This is a function that starts listening for messages from Telegram.
+async function start() {
+  bot.launch();
+}
+
+// This is a function that stops listening for messages from Telegram.
+async function stop() {
+  await bot.stop();
+}
+
+module.exports = {
+  sendFile,
+  getFile,
+  listFiles,
+  deleteFile,
+  start,
+  stop,
+};
